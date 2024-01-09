4ecfc88135f63e60f310a236884c4979a1482a62
Your Name <you@example.com>
2024-01-09 22:47:45 +0530

    change log 09-2023-01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,31 +10,31 @@ import Social from "./components/Social";
 const Home = () => {
   return (
     <div>
-      <div id="header">
+      <div id="header" className="border-b-[2px]">
         <Header />
       </div>
 
-      <div id="scroll-projects">
+      <div id="scroll-projects" className="border-b-[2px]">
         <Projects />
       </div>
 
-      <div id="scroll-experience">
+      <div id="scroll-experience" className="border-b-[2px]">
         <Experience />
       </div>
 
-      <div id="scroll-skills">
+      <div id="scroll-skills" className="border-b-[2px]">
         <Skills />
       </div>
 
-      <div id="scroll-education">
+      <div id="scroll-education" className="border-b-[2px]">
         <Education />
       </div>
 
-      <div id="scroll-footer">
+      <div id="scroll-footer" className="border-b-[2px]">
         <Footer />
       </div>
 
-      <div id="scroll-social">
+      <div id="scroll-social" className="border-b-[2px]">
         <Social />
       </div>
     </div>
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -5,3 +5,31 @@ const Skills = () => {
 };
 
 export default Skills;
+
+// // ParentComponent.js
+// import React from 'react';
+// import Person from './Person';
+
+// const ParentComponent = () => {
+//   return (
+//     <div>
+//       <Person name="John" />
+//       <Person name="Jane" />
+//       <Person name="Doe" />
+//     </div>
+//   );
+// };
+
+// // Person.js (ChildComponent)
+// import React from 'react';
+
+// const Person = (props) => {
+//   return (
+//     <p>Name: {props.name}</p>
+//   );
+// };
+
+// export default Person;
+
+// const root = ReactDOM.createRoot(document.getElelementById("root"));
+// root.render(<FavoriteColor />);
