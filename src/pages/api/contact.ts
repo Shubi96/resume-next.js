ef485cc86e1487dddef2dfb9698c6b7e922f67d0
Your Name <you@example.com>
2024-01-11 19:19:41 +0530

    fixes

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -23,6 +23,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method === "GET") {
+    res.status(200).json({ success: true, message: "OK aaso" });
+  }
   if (req.method === "POST") {
     const { email, message } = req.body;
 
