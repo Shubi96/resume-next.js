450d2c778a06bf35f1e067bae3d1cf71464800c5
Your Name <you@example.com>
2024-01-13 00:48:36 +0530

    fixes

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,154 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  AppBar,
+  Toolbar,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  IconButton,
+} from "@mui/material";
+import ContactModal from "./ContactModal";
+import MenuIcon from "@mui/icons-material/Menu";
+const sections = [
+  { id: "home", label: "Home" },
+  { id: "education", label: "Education" },
+  { id: "experience", label: "Experience" },
+  { id: "skills", label: "Skills" },
+  { id: "projects", label: "Projects" },
+];
+
+const Navbar = () => {
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const scrollToSection = (id: string) => {
+    const divId = "scroll-" + id;
+    const element = document.getElementById(divId);
+    if (element) {
+      const offset = 75;
+      const bodyRect = document.body.getBoundingClientRect().top;
+      const elementRect = element.getBoundingClientRect().top;
+      const elementPosition = elementRect - bodyRect;
+      const offsetPosition = elementPosition - offset;
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    } else {
+      console.log("div not found");
+    }
+  };
+
+  const handleContactUs = () => {
+    setOpen(true);
+  };
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+
+  const renderDrawerItems = () => {
+    return (
+      <List sx={{ minWidth: 250, mt: 2 }}>
+        {sections.map((section) => (
+          <ListItem
+            button
+            key={section.id}
+            onClick={() => handleDrawerItemClick(section.id)}
+          >
+            {section.label}
+          </ListItem>
+        ))}
+      </List>
+    );
+  };
+
+  const handleDrawerItemClick = (id: string) => {
+    scrollToSection(id);
+    handleDrawerClose();
+  };
+
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          {/* Hamburger Icon for Drawer */}
+
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerOpen}
+            sx={{ mr: 2, display: { md: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <nav className="w-full  flex items-center p-4 lg:px-10 gap-4 hidden md:flex">
+              {sections.map((section) => (
+                <a
+                  href={`#${section.id}`}
+                  key={section.id}
+                  onClick={() => scrollToSection(section.id)}
+                >
+                  <span className="cursor-pointer text-white hover:bg-blue-500 px-4 py-2 rounded-md transform hover:scale-105">
+                    {section.label}
+                  </span>
+                </a>
+              ))}
+            </nav>
+          </Typography>
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "#001f3f",
+              color: "#ffffff",
+              transition: "background-color 0.3s, color 0.3s",
+            }}
+            onClick={handleContactUs}
+          >
+            Contact Us
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        anchor="left"
+        open={isDrawerOpen}
+        onClose={handleDrawerClose}
+        sx={{
+          width: "80%", // Adjust the width as needed
+          marginTop: "75px", // Add top margin to avoid overlapping with AppBar
+        }}
+        variant="temporary"
+      >
+        {renderDrawerItems()}
+      </Drawer>
+
+      <Toolbar />
+
+      {/* Desktop Navbar */}
+
+      <div style={{ height: 75 }}></div>
+
+      {/* Contact Modal */}
+      <ContactModal
+        isOpen={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      />
+    </>
+  );
+};
+
+export default Navbar;
--- src/app/components/Social.js
+++ src/app/components/Social.js
@@ -1,50 +1,54 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import React from "react";
-import { FaGithub, FaTwitter, FaFacebookSquare } from "react-icons/fa";
+import {
+  IconButton,
+  Typography,
+  Slide,
+  AppBar,
+  Toolbar,
+  Box,
+  Grid,
+  Paper,
+} from "@mui/material";
+import { LinkedIn, GitHub, Twitter, Facebook } from "@mui/icons-material";
 
 const Social = () => {
+  const socialLinks = [
+    { icon: <LinkedIn />, text: "LinkedIn", color: "blue", href: "" },
+    { icon: <GitHub />, text: "GitHub", color: "gray", href: "" },
+    { icon: <Twitter />, text: "Twitter", color: "blue", href: "" },
+    { icon: <Facebook />, text: "Facebook", color: "blue", href: "" },
+  ];
+
   return (
-    <div className="container mx-auto p-8">
-      <div className="lg:flex gap-8">
-        <div className="mb-8 lg:w-1/2">
+    <div id="experience" className="container mx-auto p-8">
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
           <h1 className="text-left text-3xl font-bold mb-4 lg:mb-6">
-            Social Links
+            Experience
           </h1>
-          <div className="flex items-center space-x-4">
-            <div className="flex items-center space-x-2">
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                size="lg"
-                className="text-blue-500 hover:text-blue-700"
-              />
-              <h2 className="font-bold text-xl">LinkedIn</h2>
-            </div>
-            <div className="flex items-center space-x-2">
-              <FaGithub
-                size="1.5em"
-                className="text-gray-800 hover:text-gray-600"
-              />
-              <h2 className="font-bold text-xl">GitHub</h2>
-            </div>
-            <div className="flex items-center space-x-2">
-              <FaTwitter
-                size="1.5em"
-                className="text-blue-400 hover:text-blue-600"
-              />
-              <h2 className="font-bold text-xl">Twitter</h2>
-            </div>
-            <div className="flex items-center space-x-2">
-              <FaFacebookSquare
-                size="1.5em"
-                className="text-blue-700 hover:text-blue-900"
-              />
-              <h2 className="font-bold text-xl">Facebook</h2>
-            </div>
-          </div>
-        </div>
+          <Grid container spacing={2} alignItems="center">
+            {socialLinks.map((link, index) => (
+              <Grid item key={index}>
+                <IconButton
+                  color="inherit"
+                  aria-label={link.text}
+                  component="a"
+                  href={link.href} // Replace this with your actual social media URLs
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: link.color }}
+                >
+                  {link.icon}
+                </IconButton>
+                <Typography variant="subtitle1" className="font-bold">
+                  {link.text}
+                </Typography>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
         {/* Add any additional content or styling for the right side of the social component */}
-      </div>
+      </Grid>
     </div>
   );
 };
